abbc5c0accbb89c6a90a2a8edf23ba7de6c32f02
ckflight <cnkkskn001@gmail.com>
2025-09-30 11:30:18 +0300

    I have updated adc.vhd code and fir ip core will be implemented

--- FMCW3.srcs/sources_1/new/adc.vhd
+++ FMCW3.srcs/sources_1/new/adc.vhd
@@ -1,55 +1,101 @@
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity adc is
-    Port (
-        clk       : in  std_logic;                       -- 40 MHz ADC clock
-        rst       : in  std_logic;
-        enable    : in  std_logic;                       -- sampling enable from top module
-        adc_data  : in  std_logic_vector(11 downto 0);  -- ADC multiplexed bus
-        adc_of    : in  std_logic_vector(1 downto 0);   -- overflow/underflow per channel
-        data_a    : out std_logic_vector(15 downto 0);  -- signed channel A
-        data_b    : out std_logic_vector(15 downto 0);  -- signed channel B
-        of_a      : out std_logic;                       -- overflow A
-        of_b      : out std_logic                        -- overflow B
+    Port( 
+        clk : in STD_LOGIC;
+        adc_data : in STD_LOGIC_VECTOR (11 downto 0);
+        data_a : out STD_LOGIC_VECTOR (15 downto 0);
+        data_b : out STD_LOGIC_VECTOR (15 downto 0);
+        valid : out STD_LOGIC
     );
 end adc;
 
 architecture Behavioral of adc is
-    signal a_reg, b_reg : std_logic_vector(15 downto 0);
+
+    constant generate_fir : boolean := true;
+    signal data_a_buffer, data_b_buffer : std_logic_vector(11 downto 0);
+
+    COMPONENT fir_compiler_0
+    PORT (
+        aclk : IN STD_LOGIC;
+        s_axis_data_tvalid : IN STD_LOGIC;
+        s_axis_data_tready : OUT STD_LOGIC;
+        s_axis_data_tdata : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        m_axis_data_tvalid : OUT STD_LOGIC;
+        m_axis_data_tdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
+        );
+    END COMPONENT;
+    
+    signal fir_data_in_valid : std_logic := '1';
+    signal fir_ready : std_logic;
+    signal fir_data_in, fir_data_out : std_logic_vector(31 downto 0);
+    
+    signal fir_a, fir_b : std_logic_vector(15 downto 0);
+    
 begin
 
-    -- Rising edge → Channel A
-    process(clk, rst)
+    rising : process(clk, adc_data)    
     begin
-        if rst = '1' then
-            a_reg <= (others => '0');
-            of_a  <= '0';
-        elsif rising_edge(clk) then
-            if enable = '1' then
-                a_reg <= std_logic_vector(resize(signed(adc_data),16));
-                of_a  <= adc_of(0);
-            end if;
-        end if;
+        if rising_edge(clk) then
+            data_a_buffer <= adc_data;
+        end if;        
     end process;
-
-    -- Falling edge → Channel B
-    process(clk, rst)
-    begin
-        if rst = '1' then
-            b_reg <= (others => '0');
-            of_b  <= '0';
-        elsif falling_edge(clk) then
-            if enable = '1' then
-                b_reg <= std_logic_vector(resize(signed(adc_data),16));
-                of_b  <= adc_of(1);
-            end if;
-        end if;
+        
+    falling : process(clk, adc_data)    
+    begin    
+        if falling_edge(clk) then
+            data_b_buffer <= adc_data;
+        end if;        
     end process;
-
-    -- Output assignments
-    data_a <= a_reg;
-    data_b <= b_reg;
-
+        
+    output : process(clk)
+    begin    
+        if rising_edge(clk) then
+            data_a <= fir_a;
+            data_b <= fir_b;
+        end if;        
+    end process;
+    
+    fir_data_in <= "0000"&data_a_buffer&"0000"&data_b_buffer;
+    fir_a <= fir_data_out(31 downto 16);
+    fir_b <= fir_data_out(15 downto 0);
+    
+    -- Fir filter is used
+    g_fir : if generate_fir generate
+    fir : fir_compiler_0
+    PORT MAP (
+        aclk => clk,
+        s_axis_data_tvalid => fir_data_in_valid,
+        s_axis_data_tready => fir_ready,
+        s_axis_data_tdata => fir_data_in,
+        m_axis_data_tvalid => valid,
+        m_axis_data_tdata => fir_data_out
+    );                 
+    end generate;
+    
+    -- If fir is not selected then valid pulse is generated at every 41 cycles
+    -- ADC is still sampled with 40msps but 1msps valid is generated
+    g_not_fir : if not generate_fir generate
+    
+        fir_data_out <= fir_data_in;
+        
+        process(clk)
+        variable count : unsigned(7 downto 0) := (others => '0');
+        begin        
+            if rising_edge(clk) then
+                if count = to_unsigned(40, 8) then
+                    count := (others => '0');
+                    valid <= '1';
+                else
+                    count := count + 1;
+                    valid <= '0';
+                end if;
+            end if;
+        end process;
+        
+    end generate;
+    
 end Behavioral;
